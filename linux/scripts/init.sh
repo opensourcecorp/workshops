6bb94c9311937508fd12c1df15d3710190df9ce3
Ryan J. Price <ryapric@gmail.com>
2023-02-26 20:17:41 -0600

    Got basic admin service running

--- linux/scripts/init.sh
+++ linux/scripts/init.sh
@@ -6,3 +6,32 @@ mkdir -p "${wsroot}"
 
 # All directories are expected to have landed in /tmp
 cp -r /tmp/{scripts,services,instructions} "${wsroot}"/
+rm -rf /tmp/{scripts,services,instructions}
+
+apt-get update
+apt-get install -y \
+  git \
+  golang \
+  sqlite3
+
+# Set up systemd timer(s) & service(s)
+cp "${wsroot}"/services/linux-workshop-admin.* /etc/systemd/system/
+systemctl daemon-reload
+systemctl disable linux-workshop-admin.service
+systemctl enable linux-workshop-admin.timer
+systemctl start linux-workshop-admin.timer
+
+sqlite3 "${wsroot}"/main.db '
+CREATE TABLE IF NOT EXISTS score (
+  timestamp TIMESTAMP,
+  value INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS step (
+  current_step INTEGER
+);
+'
+
+# Dump the first instruction to the team's homedir
+first_instruction=$(find "${wsroot}"/instructions -name '*.md' | sort | head -n1)
+cp "${first_instruction}" /home/admin/
---
+++ linux/scripts/linux-workshop-admin.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+################################################################################
+# This is the core admin service that will be run on each workshop machine. It
+# determines & accrues the team's score, provides the next set of instructions
+# to the team when appropriate, etc. It is to be placed at
+# '/.ws/linux-workshop-admin'
+################################################################################
+
+wsroot='/.ws'
+db="${wsroot}/main.db"
+
+accrue-points() {
+  sqlite3 "${db}" "INSERT INTO score VALUES (DATETIME(), (100 * ${which_step}));"
+}
+
+# TODO: we need to figure out either under what conditions to call this, OR to
+# find a way to make it idempotent if the team hasn't finished the step they're
+# currently on
+score-for-step() {
+  which_step="${1:-}" # "$(sqlite3 ${db} 'SELECT (current_step + 1) FROM step;')"
+  if [[ ! -f "/home/admin/step_${which_step}.md" ]]; then
+    cp "${wsroot}/instructions/${which_step}.md" "/home/admin/step_${which_step}.md"
+  fi
+  sqlite3 "${db}" "UPDATE step SET current_step = ${which_step};"
+  accrue-points "${which_step}"
+}
+
+###
+# Scoring checks
+###
+
+check-binary-built() {
+  if [[ -f /opt/app/app ]] ; then
+    score-for-step 1
+  fi
+}
+
+check-symlink() {
+  if [[ -L /usr/local/bin/run-app ]] ; then
+    score-for-step 2
+  fi
+}
+
+check-systemd-service-running() {
+  if systemctl is-active app.service ; then
+    score-for-step 3
+  fi
+}
+
+check-firewall-rules() {
+  if [[ 0 -eq 1 ]] ; then
+    score-for-step 4
+  fi
+}
+
+###
+# Main wrapper def & callable for scorables
+###
+
+_main() {
+  check-binary-built
+  check-symlink
+  check-systemd-service-running
+  check-firewall-rules
+}
+
+_main
