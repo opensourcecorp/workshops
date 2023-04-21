682b6d88dba35889c1fe7c43ea115ace816e7352
Ryan J. Price <ryapric@gmail.com>
2023-04-21 13:39:46 -0500

    ALSO generate .pyi stubs, because I guess you can do that

--- protobuf-grpc/clients/client.py
+++ protobuf-grpc/clients/client.py
@@ -16,16 +16,13 @@ import pb.echo.v1.echo_pb2_grpc as echo_pb2_grpc
 import pb.employees.v1.employees_pb2 as employees_pb2
 import pb.employees.v1.employees_pb2_grpc as employees_pb2_grpc
 
+# Note that if you did NOT specify --pyi_out in the protoc call, you won't get
+# editor help like tab completion for the generated code -- everything comes out
+# as a metaclass unless you also generate those .pyi files
 if __name__ == "__main__":
     with grpc.insecure_channel("127.0.0.1:8080") as channel:
         echo_stub = echo_pb2_grpc.EchoServiceStub(channel)
 
-        # This is pretty infuriating -- Python protobuf code isn't generated
-        # directly, it generates a *Python* generator to generate the code at
-        # runtime (in *_pb2.py). As such, you don't get completion hints in
-        # editors because there's no data types to autofill etc. So, look at
-        # your proto file and determine how you should be setting up your
-        # requests!
         echo_request = echo_pb2.EchoRequest(msg = "Hello, gRPC!")
         echo_response = echo_stub.Echo(echo_request)
         print(echo_response)
@@ -35,6 +32,9 @@ if __name__ == "__main__":
         list_response = employees_stub.ListEmployees(list_request)
         print(list_response)
 
-        get_request = employees_pb2.GetEmployeeRequest(short_name = sys.argv[1])
-        get_response = employees_stub.GetEmployee(get_request)
-        print(get_response)
+        if len(sys.argv) < 2:
+            print("warning: not calling GetEmployee() because you must provide an employee's short name on the CLI. Did you look at the output of the ListEmployees() call?")
+        else:
+            get_request = employees_pb2.GetEmployeeRequest(short_name = sys.argv[1])
+            get_response = employees_stub.GetEmployee(get_request)
+            print(get_response)
